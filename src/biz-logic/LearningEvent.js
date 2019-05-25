05ecb8b95d1b16fef0efff3431a6921cbef1bb54
jpratt-mru <jpratt@mtroyal.ca>
2019-05-25 17:39:48 +0000

    Bye-bye dead code.

--- src/biz-logic/LearningEvent.js
+++ src/biz-logic/LearningEvent.js
@@ -1,14 +1,3 @@
-// import SectionCleaner from "./SectionCleaner";
-// import SubjectAbbrCleaner from "./SubjectAbbrCleaner";
-// import CourseCleaner from "./CourseCleaner";
-// import CourseNumberCleaner from "./CourseNumberCleaner";
-// import RoomCleaner from "./RoomCleaner";
-// import CapacityCleaner from "./CapacityCleaner";
-// import EndTimeCleaner from "./EndTimeCleaner";
-// import StartCleaner from "./StartCleaner";
-// import EndCleaner from "./EndCleaner";
-// import InstructorNameCleaner from "./InstructorNameCleaner";
-
 /**
  * Represents an event - not in the CS sense of the award, but as in
  * "let's go to the pavillion, I hear there's an event going on".
@@ -21,7 +10,6 @@
  * with many of their attributes not actually displayed - but they're
  * there behind the scenes, lurking.
  *
- * I use the Builder Pattern here, because I have self-confidence issues.
  */
 
 class LearningEvent {
@@ -47,39 +35,6 @@ class LearningEvent {
     this.errors = builder.errors;
     this.warnings = builder.warnings;
   }
-  // static valueOf(index, obj, startingMonday) {
-  //   const cleaners = [
-  //     new CourseCleaner(),
-  //     new SectionCleaner(),
-  //     new SubjectAbbrCleaner(),
-  //     new CourseNumberCleaner(),
-  //     new RoomCleaner(),
-  //     new CapacityCleaner(),
-  //     new StartCleaner(startingMonday),
-  //     new EndTimeCleaner(),
-  //     new EndCleaner(),
-  //     new InstructorNameCleaner()
-  //   ];
-  //   // turn all keys to lowercase
-  //   // https://stackoverflow.com/a/54985484
-  //   let lowcasedObj = Object.fromEntries(
-  //     Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v])
-  //   );
-
-  //   // add an id
-  //   lowcasedObj = Object.defineProperty(lowcasedObj, "id", {
-  //     value: index,
-  //     enumerable: true
-  //   });
-
-  //   // TODO? This *could* be a place to use reduce()....but I can't
-  //   // figure out how to yet. Grokking that damn function is HARD.
-  //   cleaners.forEach(cleaner => {
-  //     lowcasedObj = cleaner.clean(lowcasedObj);
-  //   });
-
-  //   return lowcasedObj;
-  // }
 }
 
 export default LearningEvent;
