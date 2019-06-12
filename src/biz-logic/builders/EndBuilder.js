512ceda0530e090fb08a3fefe711ecfa817a1554
jpratt-mru <jpratt@mtroyal.ca>
2019-06-12 21:29:39 +0000

    Change casing of variable.

--- src/biz-logic/builders/EndBuilder.js
+++ src/biz-logic/builders/EndBuilder.js
@@ -11,7 +11,7 @@ import TimeUtilities from "../TimeUtilities";
 class EndBuilder extends SecondaryCalculatedPropBuilder {
   static prerequisitesAreValid(prerequisites) {
     // as long as we've gotten this far, we'll have a valid
-    // startingtime and duration to work with
+    // startingTime and duration to work with
     const [start, duration] = prerequisites;
 
     return start && duration;
--- src/biz-logic/builders/StartTimeBuilder.js
+++ src/biz-logic/builders/StartTimeBuilder.js
@@ -5,12 +5,12 @@ import PrimaryPropBuilder from "./PrimaryPropBuilder";
  * a calendar. (Well, I guess it could be an all day event, but screw those guys).
  *
  * So what we'll do here is make sure that:
- *   1) there actually *is* a startingtime property to access, and
+ *   1) there actually *is* a startingTime property to access, and
  *   2) if so, it is a valid 24-hour time.
  */
 class StartTimeBuilder extends PrimaryPropBuilder {
   constructor() {
-    super("startingtime", TimeUtilities.validTime);
+    super("startingTime", TimeUtilities.validTime);
   }
 }
 
