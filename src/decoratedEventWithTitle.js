d5557c94ebd81a458aa22003911d7fd9e0894d84
jpratt-mru <jpratt@mtroyal.ca>
2019-06-14 16:19:56 +0000

    Add docs.

--- src/decoratedEventWithTitle.js
+++ src/decoratedEventWithTitle.js
@@ -1,3 +1,15 @@
+/**
+ * This adds a title property to a learningEvent - it's used by fullcalendar
+ * as the text to actually display on the calendar for a given event.
+ *
+ * Currently, we want events that look like this:
+ * COMP1501-001
+ * [jbond]
+ * E145 30/35
+ *
+ * (the 30/35 is sectioncap/roomcap)
+ */
+
 const decoratedEventWithTitle = learningEvent => {
   let titleDecoratedEvent = learningEvent;
 
